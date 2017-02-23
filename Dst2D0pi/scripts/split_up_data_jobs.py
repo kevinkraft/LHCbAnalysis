589b96c0075c8af34ebf9529fc9bca860fd84b56
kmaguire <kmaguire@cern.ch>
2017-02-23 13:57:29 +0100

    Added a dat file with the newest jan17 data. Changed the command to setup the environment as the lxplus environment has changed. Changed the script that makes the batch jobs to put the jobs in a directory and to choose different batch queues and to setup a slightly older lxplus platform on the worker node so that the old SetupProject commands still work on the worker node.

--- Dst2D0pi/scripts/split_up_data_jobs.py
+++ Dst2D0pi/scripts/split_up_data_jobs.py
@@ -2,13 +2,9 @@
 
 import os
 
-#filesperjob=50
-#filesperjob=5
 filesperjob=1
 
-#f = open('dat/data_cfg.dat')
-#f = open('/afs/cern.ch/user/k/kmaguire/Programs/KenzieAnalysisFork/Dst2D0pi/dat/analysis_cfg.dat')
-f = open('/afs/cern.ch/user/k/kmaguire/Programs/KenzieAnalysisFork/Dst2D0pi/dat/test.dat')
+f = open('/afs/cern.ch/user/k/kmaguire/Programs/KenzieAnalysisFork/Dst2D0pi/dat/jan17_full.dat')
 
 file_lines = []
 
@@ -16,30 +12,32 @@ for line in f.readlines():
   if not line.startswith('itype'): continue
   file_lines.append(line)
 
-#bq = 'test'
+bq = 'test'
 #bq = '8nm'
 #bq = '1nh'
-bq = '8nh'
+#bq = '8nh'
 #bq = '1nd'
 jobsfolder = 'data_jobs_'+bq
 
-#jobsreq = len(file_lines)/filesperjob + 1
 jobsreq = len(file_lines)
 
 print jobsreq, ' ', filesperjob, ' ', len(file_lines)
 
-#jobsreq = 1
+for j in range(0, jobsreq):
 
-for j in range(jobsreq):
+  submit = True
 
-  df = open('%s/'%(os.getcwd())+jobsfolder+'/data_j%d.dat'%(j),'w')
   for i in range(j*filesperjob, j*filesperjob + filesperjob ):
     if i > len(file_lines)-1: break
-
+    #if '?svcClass=lhcbUser' in file_lines[i]:
+    #if 'cnaf' in file_lines[i]:
+      #print 'Job {0} has the extra XROOTD suffix'.format(j)
+      #print 'Job {0} is cnaf'.format(j)
+    submit = True
+    df = open('%s/'%(os.getcwd())+jobsfolder+'/data_j%d.dat'%(j),'w')
     df.write(file_lines[i])
+    df.close()
 
-  df.close()
- 
   sfname = '%s/'%(os.getcwd())+jobsfolder+'/data_j%d.sh'%(j)
   sf = open(sfname,'w')
   sf.write('touch %s.done\n'%sfname)
@@ -48,8 +46,8 @@ for j in range(jobsreq):
   
   sf.write('mkdir -p scratch\n')
   sf.write('cd scratch\n')
-  sf.write('. LbLogin.sh\n')
-  sf.write('. SetupProject DaVinci v41r3\n')
+  sf.write('LbLogin.sh -c x86_64-slc6-gcc48-opt\n')
+  sf.write('SetupProject DaVinci v41r3\n')
   sf.write('cp %s/bin/Dst2D0pi_Analysis .\n'%os.getcwd())
   sf.write('cp %s .\n'%df.name)
   sf.write('if ( ./Dst2D0pi_Analysis -c %s -o data_j%d.root -b ) then\n'%(os.path.basename(df.name),j))
@@ -62,5 +60,6 @@ for j in range(jobsreq):
   sf.write('rm -f %s.run\n'%sfname)
   sf.close()
 
-  os.system('chmod +x %s'%sfname)
-  os.system('bsub -q '+bq+' -o %s.log %s'%(sfname,sfname))
+  if submit == True:
+    os.system('chmod +x %s'%sfname)
+    os.system('bsub -q '+bq+' -o %s.log %s'%(sfname,sfname))
