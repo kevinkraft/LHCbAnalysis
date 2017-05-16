f68b1b35291c2894a19a095a84d33ad08c8f69f4
kmaguire <kmaguire@cern.ch>
2017-05-16 17:47:25 +0200

    Added a file for making a text file list of XROOTD URLs into the .dat format for this code.

---
+++ Dst2D0pi/makeDat.py
@@ -0,0 +1,27 @@
+#script to make a .dat file from a list of root files
+#
+#Kevin Maguire
+#26/01/17
+#
+
+#---------------------------------------------------------------
+
+import os, sys
+
+#---------------------------------------------------------------
+
+ff = '/afs/cern.ch/user/k/kmaguire/cmtuser/DaVinciDev_v39r0p1/D0Lifetime/TupleMaking/magup_xrootd.txt'
+treename = 'TeslaRSTuple/DecayTree'
+fout = '/afs/cern.ch/user/k/kmaguire/Programs/KenzieAnalysisFork/Dst2D0pi/dat/may17_magup_full.dat'
+itype = '130'
+year = '2015'
+name = 'Data'
+sqrts = '13'
+
+file_out = open(fout, 'wb')
+file_in = open(ff, 'rb')
+for l in file_in.readlines():
+    if '#' in l: continue
+    l = l.replace('\n','')
+    print l
+    file_out.write( 'itype={0} sqrts={1} year={2} name={3} fname={4} tname={5}\n'.format(itype, sqrts, year, name, l, treename) )
